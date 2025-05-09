59ed9426d62367683198e67894c47d6d9271461d
Rigers Leka <Rleka@airtrade.nl>
2025-05-09 16:09:47 +0200

    create the exercise 2 (wip)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Exercise1Component } from './exercise1/exercise1.component';
+import { Exercise2Component } from "./exercise2/component/exercise2/exercise2.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Exercise1Component],
+  imports: [RouterOutlet, Exercise1Component, Exercise2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/exercise2/component/exercise2/exercise2.component.ts
+++ src/app/exercise2/component/exercise2/exercise2.component.ts
@@ -1,12 +1,12 @@
+import { CardComponent } from "../card/card.component";
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
-  selector: 'app-exercise2',
+  selector: 'exercise2',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, CardComponent],
   templateUrl: './exercise2.component.html',
   styleUrl: './exercise2.component.css'
 })
-export class Exercise2Component {
-
-}
+export class Exercise2Component { }
