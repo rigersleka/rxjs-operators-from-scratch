9d0888c3415aa0c43d2baf2a46846bfd5d5806cd
Rigers Leka <Rleka@airtrade.nl>
2025-05-09 16:10:07 +0200

    implement header implementation and countryService

---
+++ src/app/exercise2/component/header/header.component.ts
@@ -0,0 +1,29 @@
+import { Component, inject } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { Country } from '../../../country.interface';
+import { CountryService } from '../../service/country.service';
+import { map } from 'rxjs';
+
+@Component({
+  selector: 'header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  countryService = inject(CountryService)
+  countriesStore: Country[] = []
+
+  constructor() {
+    this.countryService.getCountryList()
+      .pipe(
+        map((countriesData: Country[]) => {
+          const countriesStartWithD: Country[] = countriesData.filter((c: Country) => c.country.startsWith('D'))
+
+          return countriesStartWithD
+        }))
+      .subscribe((countriesResponse: Country[]) => (this.countriesStore = countriesResponse))
+  }
+}
--- src/app/exercise2/service/country.service.ts
+++ src/app/exercise2/service/country.service.ts
@@ -1,18 +1,15 @@
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Observable, Subject, of } from 'rxjs';
 
-import { Injectable } from '@angular/core';
 import { Country } from '../../country.interface';
+import { Injectable } from '@angular/core';
+import { countries } from '../../country-mock.data';
 
-// How to emit Data using RXJs?
 @Injectable({
   providedIn: 'root'
 })
 export class CountryService {
-  // private country$: Subject<Country> = new Subject<Country>()
-  private country$: BehaviorSubject<Country> = new BehaviorSubject<Country>({
-    country: 'Albania',
-    continent: 'Europe'
-  });
+  private country$: Subject<Country> = new Subject<Country>() // emit one country in a time
+  // private country$: BehaviorSubject<Country> = new BehaviorSubject<Country>({country: 'Albania',continent: 'Europe'});
 
   setCurrentCountry(country: Country): void {
     this.country$.next(country);
@@ -21,4 +18,12 @@ export class CountryService {
   getCurrentCountry(): Observable<Country> {
     return this.country$.asObservable();
   }
+
+  getCountryList(): Observable<Country[]> {
+    return of(countries)
+  }
+
+  /* Second solution to trigger data
+    export const countryList$: Observable<Array<Country>> = of(countries)
+  */
 }
